d152db55b0dcc6cdbd932e7626a1e9d9485d1f35
Edmorin Roosevelt <edmorinroosevelt@gmail.com>
2024-02-05 22:44:15 +0100

    fix interface methode

--- etude_2/elections/model/interfaces.go
+++ etude_2/elections/model/interfaces.go
@@ -2,7 +2,7 @@ package model
 
 // Reader contains all the function to read the necessary data
 type Reader interface {
-	allPoliticians() (Politicians, error)
+	AllPoliticians() (Politicians, error)
 	PoliticianFromID(ID int) (Politician, error)
 	AllVotes() (Votes, error)
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/redmorin01/LivreGo-source
 
 go 1.21.6
-
-require github.com/redmorin01/LivreGo-source v0.0.0-20240205210008-3e5ffa098c73 // indirect
